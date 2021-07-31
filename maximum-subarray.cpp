914ba20376c229eb4a16fc10d5ec341ee2ecf1d0
Ayush Acharya <ayushjash1323@gmail.com>
2021-07-31 13:50:09 +0530

    maximum-subarray

--- maximum-subarray.cpp
+++ maximum-subarray.cpp
@@ -1,33 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxSubArray(vector<int>& nums) {
-    int best = INT_MIN, sum = 0, n = nums.size();
-    int maximum = INT_MIN;
-    for(int i = 0; i < n; ++i) {
-        if (nums[i] >= maximum) {
-            maximum = nums[i];
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int best = INT_MIN;
+        int sum = 0;
+        
+        for (int idx = 0; idx < nums.size(); ++idx) {
+            sum = max(nums[idx], sum + nums[idx]);
+            best = max(sum, best);
         }
-        sum = max(nums[i], sum + nums[i]);
-        best = max(sum, best);
+        
+        return best;
+            
+            
+        // int best = INT_MIN, sum = 0, n = nums.size();
+        // int maximum = INT_MIN;
+        // for(int i = 0; i < n; ++i) {
+        //     if (nums[i] >= maximum) {
+        //         maximum = nums[i];
+        //     }
+        //     sum = max(nums[i], sum + nums[i]);
+        //     best = max(sum, best);
+        // }
+        // return max(maximum, best);
     }
-    return max(maximum, best);
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	
-	int n;
-	cin>>n;
-	vector<int> nums;
-	for(int i = 0; i < n; ++i) {
-		int t;
-		cin>>t;
-		nums.push_back(t);
-	}
-	cout<<maxSubArray(nums);
-	
-	return 0;
-}
+};
