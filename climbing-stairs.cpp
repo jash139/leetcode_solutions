b1842afc06daf087c41b3edcfcfc9022daf94ab9
Ayush Acharya <ayushjash1323@gmail.com>
2021-07-31 14:19:17 +0530

    climbing-stairs

--- climbing-stairs.cpp
+++ climbing-stairs.cpp
@@ -1,31 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-// for leetcode class based code
-// vector<int> memo;
-// Solution(): memo(50, -1) {}
-
-//for current code
-vector<int> memo(50, -1);
-
-int climbStairs(int n) {
-    if (memo[n] >= 0) return memo[n];
-    if (n == 1) return 1;
-    if (n == 2) return 2;
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int idx = 3; idx <= n; ++idx)
+            dp[idx] = dp[idx - 1] + dp[idx - 2];
+        
+        return dp[n];
+    }
     
-    int steps = climbStairs(n - 1) + climbStairs(n - 2);
-    memo[n] = steps;
-    return steps;
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	
-	int n;
-	cin>>n;
-	cout<<climbStairs(n);
-	
-	return 0;
-}
+    
+//     vector<int> memo;
+//     Solution(): memo(50, -1) {}
+    
+//     int climbStairs(int n) {
+//         if (memo[n] >= 0) return memo[n];
+//         if (n == 1) return 1;
+//         if (n == 2) return 2;
+        
+//         int steps = climbStairs(n - 1) + climbStairs(n - 2);
+//         memo[n] = steps;
+//         return steps;
+//     }
+};
