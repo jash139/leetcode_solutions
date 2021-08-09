71989e114535c295632ea3255c074b88fd749abd
Ayush Acharya <ayushjash1323@gmail.com>
2021-08-09 17:29:59 +0530

    edit-distance

---
+++ edit-distance.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> edits(word2.size() + 1, vector<int>(word1.size() + 1, 0));
+        
+        for (int i = 0; i < word2.size() + 1; ++i)
+            for (int j = 0; j < word1.size() + 1; ++j)
+                edits[i][j] = i + j;
+        
+        for (int i = 1; i < word2.size() + 1; ++i) {
+            for (int j = 1; j < word1.size() + 1; ++j) {
+                if (word2[i - 1] == word1[j - 1])
+                    edits[i][j] = edits[i - 1][j - 1];
+                else
+                    edits[i][j] = 1 + min(edits[i - 1][j - 1], min(edits[i - 1][j], edits[i][j - 1]));
+            }
+        }
+        return edits[word2.size()][word1.size()];
+    }
+};
