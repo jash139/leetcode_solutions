3307882869457955f1eafd920b1adf6081c01bd6
Ayush Acharya <ayushjash1323@gmail.com>
2021-08-15 00:55:59 +0530

    word-break

--- word-break.cpp
+++ word-break.cpp
@@ -4,21 +4,20 @@ public:
         if (wordDict.size() == 0)
             return false;
         
-        unordered_set<string> dict;
+        unordered_set<string> dictionary;
+        for (auto word : wordDict)
+            dictionary.insert(word);
         
-        for (auto word : wordDict) 
-            dict.insert(word);
-        
-        vector<bool> dp(s.size() + 1,false);
+        vector<bool> dp(s.size() + 1, false);
         dp[0] = true;
         
-        for (int i = 1; i <= s.size(); i++) {
-            for (int j = i - 1; j >= 0; j--) {
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = i - 1; j >= 0; --j) {
                 if (dp[j]) {
-                    string word = s.substr(j, i - j);
-                    if (dict.find(word) != dict.end()) {
+                    string curWord = s.substr(j, i - j);
+                    if (dictionary.find(curWord) != dictionary.end()) {
                         dp[i] = true;
-                        break; 
+                        break;
                     }
                 }
             }
