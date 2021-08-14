2dd64d55f64c2d018aada96b629479137d03e2a3
Ayush Acharya <ayushjash1323@gmail.com>
2021-08-15 00:06:07 +0530

    unique-paths

--- unique-paths.cpp
+++ unique-paths.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> numberOfWays;
-        
-        for (int i = 0; i <= n; ++i) {
-            numberOfWays.push_back(vector<int>{});
-            for (int j = 0; j <= m; ++j) {
-                numberOfWays[i].push_back(0);
-            }
-        }
+        vector<vector<int>> numberOfWays(n + 1, vector<int>(m + 1, 0));
         
         for (int widthIdx = 1; widthIdx <= m; ++widthIdx) {
             for (int heightIdx = 1; heightIdx <= n; ++heightIdx) {
