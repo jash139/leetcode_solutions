1612f9df86028585ca9620649cd7bb8179f3d81b
Ayush Acharya <ayushjash1323@gmail.com>
2021-08-15 15:42:01 +0530

    divisor-game

---
+++ divisor-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<bool> dp(n + 1, false);
+        
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j){
+                if (i % j == 0 and !dp[i - j])
+                    dp[i] = true;
+            }
+        }
+        
+        return dp[n];
+    }
+};
