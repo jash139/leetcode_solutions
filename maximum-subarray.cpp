06fdf0d253e99c4d278c51d0cde2ceac019f05c0
Ayush Acharya <ayushjash1323@gmail.com>
2021-08-15 13:02:54 +0530

    maximum-subarray

--- maximum-subarray.cpp
+++ maximum-subarray.cpp
@@ -1,29 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int best = INT_MIN;
         int sum = 0;
+        int best = INT_MIN;
         
         for (int idx = 0; idx < nums.size(); ++idx) {
-            sum = max(nums[idx], sum + nums[idx]);
-            best = max(sum, best);
+            sum = max(sum + nums[idx], nums[idx]);
+            best = max(best, sum);
         }
         
         return best;
-            
-            
-        // int best = INT_MIN, sum = 0, n = nums.size();
-        // int maximum = INT_MIN;
-        // for(int i = 0; i < n; ++i) {
-        //     if (nums[i] >= maximum) {
-        //         maximum = nums[i];
-        //     }
-        //     sum = max(nums[i], sum + nums[i]);
-        //     best = max(sum, best);
-        // }
-        // return max(maximum, best);
     }
 };
