5d748545a917d23b9ece2df040601e2986454b56
Ayush Acharya <ayushjash1323@gmail.com>
2021-08-16 14:45:28 +0530

    word-search

--- word-search.cpp
+++ word-search.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>> &board, int i, int j, string word) {
-        if (word.size() == 0)
-            return true;
-        if (i < 0 or i >= board.size() or  j < 0 or j >= board[0].size() or board[i][j] != word[0])
-            return false;
-        
-        char ch = board[i][j];
-        board[i][j] = '*';
-        string substr = word.substr(1);
-        
-        bool returnValue = dfs(board, i - 1, j, substr) or dfs(board, i + 1, j, substr) or dfs(board, i, j - 1, substr) or dfs(board, i, j + 1, substr);
-        
-        board[i][j] = ch;
-        return returnValue;
-    }
+     bool dfs(vector<vector<char>> &board, int row, int col, string word, int curIdx) {
+         if (curIdx == word.size())
+             return true;
+         if (row < 0 or col < 0 or row >= board.size() or col >= board[0].size() or board[row][col] == '*')
+             return false;
+         
+         if (board[row][col] == word[curIdx]) {
+             board[row][col] = '*';
+             bool returnValue =
+                 dfs(board, row + 1, col, word, curIdx + 1) or 
+                 dfs(board, row - 1, col, word, curIdx + 1) or 
+                 dfs(board, row, col + 1, word, curIdx + 1) or 
+                 dfs(board, row, col - 1, word, curIdx + 1);
+                 
+             board[row][col] = word[curIdx];
+             return returnValue;
+         } else {
+             return false;
+         }
+         
+     }
     
     bool exist(vector<vector<char>>& board, string word) {
         for (int i = 0; i < board.size(); ++i) {
-            for (int j = 0; j < board[i].size(); ++j) {
-                if (dfs(board, i, j, word))
+            for (int j = 0; j < board[0].size(); ++j) {
+                if (dfs(board, i, j, word, 0)) {
                     return true;
+                }
             }
         }
         return false;
