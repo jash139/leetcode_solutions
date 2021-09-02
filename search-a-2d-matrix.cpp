33b088fcd6dfae80b186ea5f67834a04017c69df
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-02 16:43:07 +0530

    search-a-2d-matrix

---
+++ search-a-2d-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = 0;
+        int col = matrix[0].size() - 1;
+        
+        while (row < matrix.size() and col >= 0) {
+            if (matrix[row][col] < target) {
+                row++;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
