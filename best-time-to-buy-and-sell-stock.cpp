f4de4265a3242ceaeeb666308f33a8cb8186b79a
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-03 15:05:16 +0530

    best-time-to-buy-and-sell-stock

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -1,33 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int maxProfit(vector<int>& prices) {
-    int minPrice = INT_MAX, profit = 0, n = prices.size();
-    for(int i = 0; i < n; ++i) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else {
-            profit = max(profit, prices[i] - minPrice);
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        
+        for (int i = 1; i < prices.size(); ++i) {
+            int curProfit = prices[i] - minPrice;
+            maxProfit = max(maxProfit, curProfit);
+            minPrice = min(minPrice, prices[i]);
         }
+        
+        return maxProfit;
+        
+//         int minPrice = INT_MAX, profit = 0, n = prices.size();
+//         for(int i = 0; i < n; ++i) {
+//             if (prices[i] < minPrice) {
+//                 minPrice = prices[i];
+//             } else {
+//                 profit = max(profit, prices[i] - minPrice);
+//             }
+//         }
+        
+//         return profit;   
     }
-    
-    return profit;   
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	
-	int n;
-	cin>>n;
-	vector<int> prices;
-	for(int i = 0; i < n; ++i) {
-		int t;
-		cin>>t;
-		prices.push_back(t);
-	}
-	cout<<maxProfit(prices);
-	
-	return 0;
-}
+};
