813093f557def2268c8bbe045832b280a877e2a3
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-06 23:35:17 +0530

    first-missing-positive

--- first-missing-positive.cpp
+++ first-missing-positive.cpp
@@ -7,7 +7,6 @@ public:
     // That is the smallest missing positive number
     
     int firstMissingPositive(vector<int>& nums) {
-        int missingNum = 1;
         for (int idx = 0; idx < nums.size(); ++idx) {
             if (nums[idx] <= 0 or nums[idx] > nums.size() or nums[idx] == idx + 1 or nums[nums[idx] - 1] == nums[idx])
                 continue;
