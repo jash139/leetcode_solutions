8af694feb8b0bb059ab4225be9923e314369798c
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-07 19:46:25 +0530

    binary-search-tree-iterator

--- binary-search-tree-iterator.cpp
+++ binary-search-tree-iterator.cpp
@@ -10,33 +10,69 @@
  * };
  */
 class BSTIterator {
-    vector<TreeNode*> inorder;
-    int pos;
+    stack<TreeNode*> nodesStack;
 public:
-    BSTIterator(TreeNode* root) {
-        inorderTraverseNodes(root);
-        pos = -1;
+    BSTIterator(TreeNode *root) {
+        pushLeftNodesToStack(root);
     }
-    
-    void inorderTraverseNodes(TreeNode* currNode) {
-        if (currNode == nullptr)
-            return;
-        inorderTraverseNodes(currNode -> left);
-        inorder.push_back(currNode);
-        inorderTraverseNodes(currNode -> right);
-    }
-    
+
     int next() {
-        pos++;
-        return inorder[pos] -> val;
+        TreeNode* currNode = nodesStack.top();
+        nodesStack.pop();
+        
+        if (currNode -> right != nullptr)
+            pushLeftNodesToStack(currNode -> right);
+            
+        return currNode -> val;
     }
     
     bool hasNext() {
-        if (pos + 1 < inorder.size())
-            return true;
+        if (nodesStack.empty())
+            return false;
         
-        return false;
+        return true;
     }
+    
+    void pushLeftNodesToStack(TreeNode* root) {
+        TreeNode* currNode = root;
+        
+        while (currNode != nullptr) {
+            nodesStack.push(currNode);
+            currNode = currNode -> left;
+        }
+    }
+    
+
+----------------------------------------------------------------------
+
+    
+	vector<TreeNode*> inorder;
+	int pos;
+public:
+	BSTIterator(TreeNode* root) {
+		inorderTraverseNodes(root);
+		pos = -1;
+	}
+	
+	void inorderTraverseNodes(TreeNode* currNode) {
+		if (currNode == nullptr)
+			return;
+		inorderTraverseNodes(currNode -> left);
+		inorder.push_back(currNode);
+		inorderTraverseNodes(currNode -> right);
+	}
+	
+	int next() {
+		pos++;
+		return inorder[pos] -> val;
+	}
+	
+	bool hasNext() {
+		if (pos + 1 < inorder.size())
+			return true;
+		
+		return false;
+	}
 };
 
 /**
