3cac3096f1893b99ee29cb7c058e172665658455
Ayush Acharya <ayushjash1323@gmail.com>
2021-10-22 02:54:40 +0530

    house-robber

--- house-robber.cpp
+++ house-robber.cpp
@@ -1,20 +1,40 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    // O(n) Time | O(n) Space
-    // n : length of nums
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
         
-        vector<int> maxAmount(nums.size(), 0);
-        maxAmount[0] = nums[0];
-        maxAmount[1] = max(nums[0], nums[1]);
+        vector<int> maxPossible(nums.size(), 0);
+        maxPossible[0] = nums[0];
+        maxPossible[1] = max(nums[0], nums[1]);
         
-        for (int idx = 2; idx < nums.size(); ++idx)
-            maxAmount[idx] = max(maxAmount[idx - 1], maxAmount[idx - 2] + nums[idx]);
+        for (int i = 2; i < nums.size(); ++i) {
+            maxPossible[i] = max(nums[i] + maxPossible[i - 2], maxPossible[i - 1]);
+        }
         
-        return maxAmount[maxAmount.size() - 1];
+        return maxPossible[nums.size() - 1];
+    }
+    
+------------------------------------------------------------------------------
+    
+    // O(1) Space
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
+        
+        int prev = nums[0];
+        int curr = max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.size(); ++i) {
+            int currAmount = max(nums[i] + prev, curr);
+            prev = curr;
+            curr = currAmount;
+        }
+        
+        return curr;
     }
 };
