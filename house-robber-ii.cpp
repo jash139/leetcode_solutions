ae8dda0a9a58fe2937c35c2e9d0294aad2380d85
Ayush Acharya <ayushjash1323@gmail.com>
2021-10-22 03:20:23 +0530

    house-robber-ii

--- house-robber-ii.cpp
+++ house-robber-ii.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
-        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
         
-        int prev1 = 0, cur1 = 0;
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            int temp = prev1;
-            prev1 = cur1;
-            cur1 = max(temp + nums[i], cur1);
+        int withFirstHouse = robHouseInRange(nums, 0, nums.size() - 1);
+        int withoutFirstHouse = robHouseInRange(nums, 1, nums.size());
+        return max(withFirstHouse, withoutFirstHouse);
+    }
+    
+    int robHouseInRange(vector<int> &nums, int start, int end) {
+        int prev = 0;
+        int curr = 0;
+        int maxAmount = 0;
+        
+        for (int i = start; i < end; ++i) {
+            maxAmount = max(prev + nums[i], curr);
+            prev = curr;
+            curr = maxAmount;
         }
         
-        int prev2 = 0, cur2 = 0;
-        for (int i = 1; i < nums.size(); ++i) {
-            int temp = prev2;
-            prev2 = cur2;
-            cur2 = max(temp + nums[i], cur2);
-        }
-        
-        return max(cur1, cur2);
+        return maxAmount;
     }
 };
